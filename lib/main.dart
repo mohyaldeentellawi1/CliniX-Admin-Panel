541bc2401fc0a9da3109e4cce141756156afdd5a
Mohyaldeen <mohyaldeentellawi@.com>
2024-01-08 23:58:54 +0300

    first Steps

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:clinix_admin_panel/views/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+
+void main() {
+  runApp(const ClinixAdminPanel());
+}
+
+class ClinixAdminPanel extends StatelessWidget {
+  const ClinixAdminPanel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Clinix Admin Panel',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MainScreenView(),
+    );
+  }
+}
